0533e41c6a87d4cc50cdd0ff5938e07ceda8c5ad
Tushar Meena <89063728+tmeena202@users.noreply.github.com>
2026-02-22 07:50:45 +0530

    updated for vercel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL =
+  import.meta.env.MODE === "production"
+    ? "https://dev-tinder-phi-nine.vercel.app"
+    : "http://localhost:3000";
